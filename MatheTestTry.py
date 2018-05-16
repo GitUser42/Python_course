c3672f1784ae821b855f763346129e4e4cf9a49e
GitUser42 <philippharlos@yahoo.de>
2018-05-16 14:39:11 +0200

    selftests

---
+++ MatheTestTry.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 10 20:43:53 2018
+
+@author: Philipp
+"""
+
+print("Die Aufgabe lautet: 9+26")
+print("Bitte geben Sie Ihre Antwort ein:")
+x=input()
+print("Ihre Antwort lautet:", x)
+print("Die richtige Antwort lautet: 35")
